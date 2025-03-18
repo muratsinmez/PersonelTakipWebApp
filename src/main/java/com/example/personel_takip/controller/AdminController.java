5340d681df7f00ce72a1b673aa82d00b97459b91
Murat Sinmez <muratsinmez06@gmail.com>
2025-03-18 18:12:43 +0300

    PersonelTakipWebApp

--- src/main/java/com/example/personel_takip/controller/AdminController.java
+++ src/main/java/com/example/personel_takip/controller/AdminController.java
@@ -1,11 +1,14 @@
 package com.example.personel_takip.controller;
 
+import com.example.personel_takip.model.Personel;
 import com.example.personel_takip.service.PersonelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -14,9 +17,19 @@ public class AdminController {
     @Autowired
     private PersonelService personelService;
 
+
     @GetMapping("/dashboard")
-    public String dashboard(Model model) {
-        model.addAttribute("personeller", personelService.getAllPersonel());
+    public String showDashboard(Model model) {
+        List<Personel> personellerList = personelService.getAllPersonel();
+        model.addAttribute("personeller", personellerList);
         return "admin-dashboard";
     }
+
+    /**
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        List<Personel> personelList = personelService.getAllPersonel();
+        model.addAttribute("personeller", personelService.getAllPersonel());
+        return "admin-dashboard";
+    }*/
 }
